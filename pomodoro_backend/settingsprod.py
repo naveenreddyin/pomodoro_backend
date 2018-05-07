5f5e5d2eb1b8eda59ca3621295c146bff11d7867
Naveen <naveen@mojob.io>
2018-05-07 13:22:12 +0200

    added static files settings

--- pomodoro_backend/settingsprod.py
+++ pomodoro_backend/settingsprod.py
@@ -14,4 +14,8 @@ DATABASES = {
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
+# for static files
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
